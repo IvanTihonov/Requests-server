e797d72f708301d9ee46a6b8c14d95fd872552cd
Ivan Tihonov <iatihonov1989@yandex.ru>
2024-01-18 14:38:21 +0500

    first fixed

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Todo } from './components';
+import styles from './app.module.css';
+
+export const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((todosData) => todosData.json())
+      .then((loadedTodos) => setTodos(loadedTodos));
+  }, []);
+
+  return (
+    <div className={styles.App}>
+      <div>
+        {todos.map(({ id, title, completed }) => (
+          <Todo key={id} title={title} completed={completed} /> 
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
